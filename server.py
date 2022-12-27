a5e6363bf02d1ee2e32fcb27aa529aec094e5df9
MinhHoan147 <72193415+MinhHoan147@users.noreply.github.com>
2022-12-27 12:19:25 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,36 +1,166 @@
-def broadcast_message(connection, message_json):
-    '''Send a message to all client sockets connected to the server...ALL JSON ARE ENCODED'''
-    for client_socket in connection.client_sockets:
-        client_socket.send(message_json)
+#Server side Advanced GUI chat room (admin)
+import tkinter, socket, threading, json
+from tkinter import DISABLED, VERTICAL, END, NORMAL
+
+#Define window
+root = tkinter.Tk()
+root.title("Chat Server")
+root.iconbitmap("message_icon.ico")
+root.geometry('600x600')
+root.resizable(0,0)
+
+#Define fonts and colors
+my_font = ('SimSun', 14)
+black = "#010101"
+light_green = "#1fc742"
+root.config(bg=black)
+
+#Create a Connection class to hold our server socket
+class Connection():
+    '''A class to store a connection - a server socket and pertinent information'''
+    def __init__(self):
+        self.host_ip = socket.gethostbyname(socket.gethostname())
+        self.encoder = 'utf-8'
+        self.bytesize = 1024
+
+        self.client_sockets = []
+        self.client_ips = []
+        self.banned_ips = []
 
 
-def recieve_message(connection, client_socket):
-    '''Recive an incoming message from a client'''
-    while True:
-        #Get a message_json from a client
-        try:
-            message_json = client_socket.recv(connection.bytesize)
-            process_message(connection, message_json, client_socket)
-        except:
-            break
-  
+#Define Functions
+def start_server(connection):
+    '''Start the server on a given port number'''
+    #Get the port number to run the serrver and attach to the connection object
+    connection.port = int(port_entry.get())
 
-def self_broadcast(connection):
-    '''Broadcast a special admin message to all clients'''
-    #Create a message packet
-    message_packet = create_message("MESSAGE", "Admin (broadcast)", input_entry.get(), light_green)
+    #Create server socket
+    connection.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.server_socket.bind((connection.host_ip, connection.port))
+    connection.server_socket.listen()
+
+    #Update GUI
+    history_listbox.delete(0, END)
+    history_listbox.insert(0, f"Server started on port {connection.port}.")
+    end_button.config(state=NORMAL)
+    self_broadcast_button.config(state=NORMAL)
+    message_button.config(state=NORMAL)
+    kick_button.config(state=NORMAL)
+    ban_button.config(state=NORMAL)
+    start_button.config(state=DISABLED)
+
+    #Create a thread to continously listen for connections
+    connect_thread = threading.Thread(target=connect_client, args=(connection,))
+    connect_thread.start()
+
+
+def end_server(connection):
+    '''Begin the process of ending the server'''
+    #Alert all users that the server is closing
+    message_packet = create_message("DISCONNECT", "Admin (broadcast)", "Server is closing...", light_green)
     message_json = json.dumps(message_packet)
     broadcast_message(connection, message_json.encode(connection.encoder))
 
-    #Clear the input entry
-    input_entry.delete(0, END)
-        broadcast_message(f"{client_name} has joined the chat!".encode(ENCODER))
+    #Update GUI
+    history_listbox.insert(0, f"Server closing on port {connection.port}.")
+    end_button.config(state=DISABLED)
+    self_broadcast_button.config(state=DISABLED)
+    message_button.config(state=DISABLED)
+    kick_button.config(state=DISABLED)
+    ban_button.config(state=DISABLED)
+    start_button.config(state=NORMAL)
 
-        #Now that a new client has connected, start a thread
-        recieve_thread = threading.Thread(target=recieve_message, args=(client_socket,))
+    #Close server socket
+    connection.server_socket.close()
+
+
+def connect_client(connection):
+    '''Connect an incoming client to the server'''
+    while True:
+        try:
+            client_socket, client_address = connection.server_socket.accept()
+            #Check to see if the IP of the client is banned.
+            if client_address[0] in connection.banned_ips:
+                message_packet = create_message("DISCONNECT", "Admin (private)", "You have been banned...goodbye", light_green)
+                message_json = json.dumps(message_packet)
+                client_socket.send(message_json.encode(connection.encoder))
+
+                #Clost the client socket
+                client_socket.close()
+            else:
+                #Send a message pakcet to recieve client info
+                message_packet = create_message("INFO", "Admin (private)", "Please send your name", light_green)
+                message_json = json.dumps(message_packet)
+                client_socket.send(message_json.encode(connection.encoder))
+
+                #Wait for confimration message to be sent verifiying the connection
+                message_json = client_socket.recv(connection.bytesize)
+                process_message(connection, message_json, client_socket, client_address)
+        except:
+            break
+
+
+def create_message(flag, name, message, color):
+    '''Return a message packet to be sent'''
+    message_packet = {
+        "flag": flag,
+        "name": name,
+        "message": message,
+        "color": color,
+    }
+
+    return message_packet
+
+
+def process_message(connection, message_json, client_socket, client_address=(0,0)):
+    '''Update server information based on a message packet flag'''
+    message_packet = json.loads(message_json) #decode and turn to dict in one step!
+    flag = message_packet["flag"]
+    name = message_packet["name"]
+    message = message_packet["message"]
+    color = message_packet["color"]
+
+    if flag == "INFO":
+        #Add the new client information to the appropriate lists
+        connection.client_sockets.append(client_socket)
+        connection.client_ips.append(client_address[0])
+
+        #Broadcast the new client joining and update GUI
+        message_packet = create_message("MESSAGE", "Admin (broadcast)", f"{name} has joined the server!!!", light_green)
+        message_json = json.dumps(message_packet)
+        broadcast_message(connection, message_json.encode(connection.encoder))
+
+        #Update server UI
+        client_listbox.insert(END, f"Name: {name}        IP Addr: {client_address[0]}")
+
+        #Now that a client has been established, start a thread to recieve messages
+        recieve_thread = threading.Thread(target=recieve_message, args=(connection, client_socket,))
         recieve_thread.start()
+    
+    elif flag == "MESSAGE":
+        #Broadcast the given message
+        broadcast_message(connection, message_json)
 
-        
-#Start the server
-print("Server is listening for incoming connections...\n")
-connect_client()
+        #Update the server UI
+        history_listbox.insert(0, f"{name}: {message}")
+        history_listbox.itemconfig(0, fg=color)
+
+    elif flag == "DISCONNECT":
+        #Close/remove client socket
+        index = connection.client_sockets.index(client_socket)
+        connection.client_sockets.remove(client_socket)
+        connection.client_ips.pop(index)
+        client_listbox.delete(index)
+        client_socket.close()
+ 
+        #Alert all users that the client has left the chat
+        message_packet = create_message("MESSAGE", "Admin (broadcast)", f"{name} has left the server...", light_green)
+        message_json = json.dumps(message_packet)
+        broadcast_message(connection, message_json.encode(connection.encoder))
+
+        #Update the server UI
+        history_listbox.insert(0, f"Admin (broadcast): {name} has left the server...")
+
+    else:
+        #Catch for errors...
+        history_listbox.insert(0, "Error processing message...")
