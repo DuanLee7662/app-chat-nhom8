f7fbda4d644d2949fa8c122e64dbc8efb1d708f8
dodaiduong-abc <80965198+dodaiduong-abc@users.noreply.github.com>
2023-01-03 13:50:28 +0700

    Update server.py

--- server.py
+++ server.py
@@ -158,11 +158,11 @@ def process_message(connection, message_json, client_socket, client_address=(0,0
         message_json = json.dumps(message_packet)
         broadcast_message(connection, message_json.encode(connection.encoder))
 
-        #Update the server UI
+       #Update the server UI
         history_listbox.insert(0, f"Admin (broadcast): {name} has left the server...")
 
     else:
-        #Catch for errors...
+      #Catch for errors...
         history_listbox.insert(0, "Error processing message...")
 
         #Admin Frame Layout
@@ -177,6 +177,77 @@ def process_message(connection, message_json, client_socket, client_address=(0,0
        #Create a Connection object and run the root window's mainloop()
         my_connection = Connection()
         root.mainloop()
+
+        
+#Define GUI Layout
+#Create Frames
+        info_frame = tkinter.Frame(root, bg=black)
+        color_frame = tkinter.Frame(root, bg=black)
+        output_frame = tkinter.Frame(root, bg=black)
+        input_frame = tkinter.Frame(root, bg=black)
+
+        info_frame.pack()
+        color_frame.pack()
+        output_frame.pack(pady=10)
+        input_frame.pack()
+
+#Info Frame Layout
+        name_label = tkinter.Label(info_frame, text="Client Name:", font=my_font, fg=light_green, bg=black)
+        name_entry = tkinter.Entry(info_frame, borderwidth=3, font=my_font)
+        ip_label = tkinter.Label(info_frame, text="Host IP:", font=my_font, fg=light_green, bg=black)
+        ip_entry = tkinter.Entry(info_frame, borderwidth=3, font=my_font)
+        port_label = tkinter.Label(info_frame, text="Port Num:", font=my_font, fg=light_green, bg=black)
+        port_entry = tkinter.Entry(info_frame, borderwidth=3, font=my_font, width=10)
+        connect_button = tkinter.Button(info_frame, text="Connect", font=my_font, bg=light_green, borderwidth=5, width=10, command=lambda:connect(my_connection))
+        disconnect_button = tkinter.Button(info_frame, text="Disconnect", font=my_font, bg=light_green, borderwidth=5, width=10, state=DISABLED, command=lambda:disconnect(my_connection))
+
+        name_label.grid(row=0, column=0, padx=2, pady=10)
+        name_entry.grid(row=0, column=1, padx=2, pady=10)
+        port_label.grid(row=0, column=2, padx=2, pady=10)
+        port_entry.grid(row=0, column=3, padx=2, pady=10)
+        ip_label.grid(row=1, column=0, padx=2, pady=5)
+        ip_entry.grid(row=1, column=1, padx=2, pady=5)
+        connect_button.grid(row=1, column=2, padx=4, pady=5)
+        disconnect_button.grid(row=1, column=3, padx=4, pady=5)
+
+#Color Frame Layout
+        color = StringVar()
+        color.set(white)
+        white_button = tkinter.Radiobutton(color_frame, width=5, text="White", variable=color, value=white, bg=black, fg=light_green, font=my_font)
+        red_button = tkinter.Radiobutton(color_frame, width=5, text="Red", variable=color, value=red, bg=black, fg=light_green, font=my_font)
+        orange_button = tkinter.Radiobutton(color_frame, width=5, text="Orange", variable=color, value=orange, bg=black, fg=light_green, font=my_font)
+        yellow_button = tkinter.Radiobutton(color_frame, width=5, text="Yellow", variable=color, value=yellow, bg=black, fg=light_green, font=my_font)
+        green_button = tkinter.Radiobutton(color_frame, width=5, text="Green", variable=color, value=green, bg=black, fg=light_green, font=my_font)
+        blue_button = tkinter.Radiobutton(color_frame, width=5, text="Blue", variable=color, value=blue, bg=black, fg=light_green, font=my_font)
+        purple_button = tkinter.Radiobutton(color_frame, width=5, text="Purple", variable=color, value=purple, bg=black, fg=light_green, font=my_font)
+        color_buttons = [white_button, red_button, orange_button, yellow_button, green_button, blue_button, purple_button]
+
+        white_button.grid(row=1, column=0, padx=2, pady=2)
+        red_button.grid(row=1, column=1, padx=2, pady=2)
+        orange_button.grid(row=1, column=2, padx=2, pady=2)
+        yellow_button.grid(row=1, column=3, padx=2, pady=2)
+        green_button.grid(row=1, column=4, padx=2, pady=2)
+        blue_button.grid(row=1, column=5, padx=2, pady=2)
+        purple_button.grid(row=1, column=6, padx=2, pady=2)
+
+#Output Frame Layout
+        my_scrollbar = tkinter.Scrollbar(output_frame, orient=VERTICAL)
+        my_listbox = tkinter.Listbox(output_frame, height=20, width=55, borderwidth=3, bg=black, fg=light_green, font=my_font, yscrollcommand=my_scrollbar.set)
+        my_scrollbar.config(command=my_listbox.yview)
+
+        my_listbox.grid(row=0, column=0)
+        my_scrollbar.grid(row=0, column=1, sticky="NS")
+
+#Input Frame Layout
+        input_entry = tkinter.Entry(input_frame, width=45, borderwidth=3, font=my_font)
+        send_button = tkinter.Button(input_frame, text="send", borderwidth=5, width=10, font=my_font, bg=light_green, state=DISABLED, command=lambda:send_message(my_connection))
+        input_entry.grid(row=0, column=0, padx=5)
+        send_button.grid(row=0, column=1, padx=5)
+
+#Create a Connection object and run the root window's mainloop()
+        my_connection = Connection()
+        root.mainloop()
+
 
         
 
